d61a43a552a8d4235fbc6f102725ccc874deca5e
José Pedretti Neto <jpedretti@gmail.com>
2016-10-10 21:05:55 +0000

    added simple notification

--- WeatherForecast/app/src/main/java/com/example/jpedretti/weatherforecast/MainActivity.java
+++ WeatherForecast/app/src/main/java/com/example/jpedretti/weatherforecast/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.jpedretti.weatherforecast;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
 
 import java.text.SimpleDateFormat;
@@ -14,6 +22,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     private ViewPager viewPager;
     private ActionBar actionBar;
+    private static int notificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +33,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
-        java.util.Calendar calendar = java.util.Calendar.getInstance();
-        String weekDay;
-        SimpleDateFormat dayFormat;
-        dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
-
-        actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        actionBar.addTab(actionBar.newTab().setText("Today").setTabListener(this));
-
-        for (int i = 0; i < 5; i++){
-            calendar.add(Calendar.DAY_OF_WEEK, 1);
-            weekDay = dayFormat.format(calendar.getTime());
-            actionBar.addTab(actionBar.newTab().setText(weekDay).setTabListener(this));
-        }
+        setUpTabNavigation();
 
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -56,6 +51,67 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
             }
         });
+
+        if (notificationId == 0) {
+            postAlert(0);
+        }
+    }
+
+    private void postAlert(int i) {
+        NotificationCompat.Builder builder = getBuilder(i);
+
+        PendingIntent pendingIntent = getPendingIntent();
+
+        builder.setContentIntent(pendingIntent);
+
+        notify(builder);
+    }
+
+    private void notify(NotificationCompat.Builder builder) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationId,builder.build());
+        notificationId++;
+    }
+
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class)
+                .addNextIntent(intent);
+
+        return stackBuilder.
+                getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @NonNull
+    private NotificationCompat.Builder getBuilder(int forecastIndex) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("Weather Alert!")
+                .setContentText(WeatherData.outlookArray[forecastIndex])
+                .setSmallIcon(R.drawable.small_icon)
+                .setAutoCancel(true)
+                .setTicker("Wrap up warm!")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        WeatherData.symbolArray[forecastIndex]));
+        return builder;
+    }
+
+    private void setUpTabNavigation() {
+        Calendar calendar = Calendar.getInstance();
+        String weekDay;
+        SimpleDateFormat dayFormat;
+        dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        actionBar.addTab(actionBar.newTab().setText("Today").setTabListener(this));
+
+        for (int i = 0; i < 5; i++) {
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+            weekDay = dayFormat.format(calendar.getTime());
+            actionBar.addTab(actionBar.newTab().setText(weekDay).setTabListener(this));
+        }
     }
 
     @Override
