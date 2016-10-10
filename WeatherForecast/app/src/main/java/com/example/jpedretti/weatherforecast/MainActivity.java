7773f216ae5ec524dad0844d989134b43d924129
José Pedretti Neto <jpedretti@gmail.com>
2016-10-10 20:40:07 +0000

    added tab navigation through week days

--- WeatherForecast/app/src/main/java/com/example/jpedretti/weatherforecast/MainActivity.java
+++ WeatherForecast/app/src/main/java/com/example/jpedretti/weatherforecast/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.jpedretti.weatherforecast;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
-public class MainActivity extends FragmentActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     private ViewPager viewPager;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,53 @@ public class MainActivity extends FragmentActivity {
         viewPager = (ViewPager) findViewById(R.id.pager);
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        String weekDay;
+        SimpleDateFormat dayFormat;
+        dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        actionBar.addTab(actionBar.newTab().setText("Today").setTabListener(this));
+
+        for (int i = 0; i < 5; i++){
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+            weekDay = dayFormat.format(calendar.getTime());
+            actionBar.addTab(actionBar.newTab().setText(weekDay).setTabListener(this));
+        }
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
