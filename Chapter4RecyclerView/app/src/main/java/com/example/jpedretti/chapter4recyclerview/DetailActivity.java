c6b3ca6a4d317649c76d22eccb75eede4109bb74
José Pedretti Neto <jpedretti@gmail.com>
2016-10-13 21:35:21 +0000

    added camera capture and save captured image

--- Chapter4RecyclerView/app/src/main/java/com/example/jpedretti/chapter4recyclerview/DetailActivity.java
+++ Chapter4RecyclerView/app/src/main/java/com/example/jpedretti/chapter4recyclerview/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.jpedretti.chapter4recyclerview;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -11,8 +15,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
+
+import static com.example.jpedretti.chapter4recyclerview.MainData.detailWebLink;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -25,6 +36,24 @@ public class DetailActivity extends AppCompatActivity {
     View.OnTouchListener listener;
     private int imageIndex;
     private static final String DEBUG_TAG = "tag";
+    private static final int PREVIEW_REQUEST_CODE = 1;
+    private static final int SAVE_REQUEST_CODE = 2;
+    private String photoPath;
+    private File photoFile;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PREVIEW_REQUEST_CODE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            detailImage.setImageBitmap(imageBitmap);
+        } else if ( requestCode == SAVE_REQUEST_CODE && resultCode == RESULT_OK){
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(photoFile);
+            intent.setData(contentUri);
+            this.sendBroadcast(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,39 +62,88 @@ public class DetailActivity extends AppCompatActivity {
 
         selectedItem = getIntent().getIntExtra(MainActivity.SELECTED_ITEM_ID, -1);
 
-        detailImage = (ImageView) findViewById(R.id.detail_image);
+        setCameraLink();
 
-        TextView detailName = (TextView) findViewById(R.id.detail_name);
-
-        TextView detailDistance = (TextView) findViewById(R.id.detail_distance);
-
-        TextView detailText = (TextView) findViewById(R.id.detail_text);
-        detailText.setMovementMethod(new ScrollingMovementMethod());
-
-        ImageView detailWebLink = (ImageView) findViewById(R.id.detail_web_link);
-
-        Random n = new Random();
-        int m = n.nextInt((600 - 20) + 1) + 20;
-
-        setTitle(getString(R.string.app_name) + " - " + MainData.
-                nameArray[selectedItem]);
-        detailImage.setImageResource(MainData.detailImageArray[selectedItem]);
-        detailName.setText(MainData.nameArray[selectedItem]);
-        detailDistance.setText(String.valueOf(m) + " miles");
-        detailText.setText(MainData.detailTextArray[selectedItem]);
-
-        detailWebLink.setOnClickListener(new View.OnClickListener() {
+        ImageView cameraSave = (ImageView) findViewById(R.id.camera_save);
+        cameraSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(
-                        MainData.detailWebLink[selectedItem]));
-                startActivity(intent);
+                takePhoto();
             }
         });
 
+        setTitle(getString(R.string.app_name) + " - " + MainData.
+                nameArray[selectedItem]);
+
+        detailImage = (ImageView) findViewById(R.id.detail_image);
+        detailImage.setImageResource(MainData.detailImageArray[selectedItem]);
+
+        setDetailInfo();
+
+        setDetailWebLink();
+
+        setTouchListener();
+
+        imageIndex = 0;
+    }
+
+    private void takePhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            photoFile = null;
+            try {
+                photoFile = fileName();
+            }catch (IOException ex){
+                Toast.makeText(this, "No SD Card.", Toast.LENGTH_SHORT).show();
+            }
+
+            if(photoFile != null){
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, SAVE_REQUEST_CODE);
+            }
+        }
+    }
+
+    private File fileName() throws IOException{
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String file = MainData.nameArray[selectedItem] + "_" + time + "_";
+        File directory = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(file, ".jpg", directory);
+        photoPath = "file:" + image.getAbsolutePath();
+
+        return image;
+    }
+
+    private void setCameraLink() {
+        ImageView cameraLink = (ImageView) findViewById(R.id.camera);
+        cameraLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent,PREVIEW_REQUEST_CODE);
+                }
+            }
+        });
+    }
+
+    private void setDetailInfo() {
+        TextView detailText = (TextView) findViewById(R.id.detail_text);
+        detailText.setMovementMethod(new ScrollingMovementMethod());
+        detailText.setText(MainData.detailTextArray[selectedItem]);
+
+        TextView detailName = (TextView) findViewById(R.id.detail_name);
+        detailName.setText(MainData.nameArray[selectedItem]);
+
+        Random n = new Random();
+        int m = n.nextInt((600 - 20) + 1) + 20;
+        TextView detailDistance = (TextView) findViewById(R.id.detail_distance);
+        detailDistance.setText(String.valueOf(m) + " miles");
+    }
+
+    private void setTouchListener() {
         detector = new GestureDetector(this, new GalleryGestureDetector());
         listener = new View.OnTouchListener() {
 
@@ -74,13 +152,26 @@ public class DetailActivity extends AppCompatActivity {
                 return detector.onTouchEvent(event);
             }
         };
-
-        imageIndex = 0;
         detailImage.setOnTouchListener(listener);
     }
 
+    private void setDetailWebLink() {
+        ImageView detailWebLinkImage = (ImageView) findViewById(R.id.detail_web_link);
+        detailWebLinkImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(
+                        detailWebLink[selectedItem]));
+                startActivity(intent);
+            }
+        });
+    }
+
     private class GalleryGestureDetector extends GestureDetector.SimpleOnGestureListener {
-        private int item  = selectedItem;
+        private int item = selectedItem;
 
         @Override
         public boolean onDown(MotionEvent e) {
@@ -92,6 +183,7 @@ public class DetailActivity extends AppCompatActivity {
         public void onShowPress(MotionEvent e) {
             Log.d(DEBUG_TAG, "onShowPress");
             detailImage.setElevation(4);
+            detailImage.setBackgroundColor(Color.GRAY);
         }
 
         @Override
@@ -123,6 +215,7 @@ public class DetailActivity extends AppCompatActivity {
                 }
             }
             detailImage.setElevation(0);
+            detailImage.setBackgroundColor(Color.TRANSPARENT);
             return true;
         }
     }
