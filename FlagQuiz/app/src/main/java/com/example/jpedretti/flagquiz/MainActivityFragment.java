69fdb198f9784f518941d438b89581ffcaf3c74a
jpedretti <jpedretti@gmail.com>
2016-12-02 15:59:28 -0200

    quiz ready, settings must be implemented yet

--- FlagQuiz/app/src/main/java/com/example/jpedretti/flagquiz/MainActivityFragment.java
+++ FlagQuiz/app/src/main/java/com/example/jpedretti/flagquiz/MainActivityFragment.java
@@ -1,22 +1,321 @@
 package com.example.jpedretti.flagquiz;
 
-import android.support.v4.app.Fragment;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
-    }
+    // String used when logging error messages
+    private static final String TAG = "FlagQuiz Activity";
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList; // flag file names
+    private List<String> quizCountriesList; // countries in current quiz
+    private Set<String> regionsSet; // world regions in current quiz
+    private String correctAnswer; // correct country for the current flag
+    private int totalGuesses; // number of guesses made
+    private int correctAnswers; // number of correct guesses
+    private int guessRows; // number of rows displaying guess Buttons
+    private SecureRandom random; // used to randomize the quiz
+    private Handler handler; // used to delay loading next flag
+    private Animation shakeAnimation; // animation for incorrect guess
+
+    private LinearLayout quizLinearLayout; // layout that contains the quiz
+    private TextView questionNumberTextView; // shows current question #
+    private ImageView flagImageView; // displays a flag
+    private LinearLayout[] guessLinearLayouts; // rows of answer Buttons
+    private TextView answerTextView; // displays correct answer
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        //load the shake animation that's used for incorrect answer
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3);
+
+        // get references to GUI components
+        quizLinearLayout =
+                (LinearLayout) view.findViewById(R.id.quizLinearLayout);
+        questionNumberTextView =
+                (TextView) view.findViewById(R.id.questionNumberTextView);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+        guessLinearLayouts = new LinearLayout[4];
+        guessLinearLayouts[0] =
+                (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+        guessLinearLayouts[1] =
+                (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+        guessLinearLayouts[2] =
+                (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+        guessLinearLayouts[3] =
+                (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+        answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+
+        //configure listener for the guess buttons
+        for (LinearLayout row : guessLinearLayouts) {
+            for (int column = 0; column < row.getChildCount(); column++) {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
+
+        //set questionNumberTextView's text
+        questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+
+        return view;
+    }
+
+    //update guessRows based on value in SharedPreferences
+    public void updateGuessRows(SharedPreferences sharedPreferences) {
+        //get the number of guess buttons that should be displayed
+        String choices = sharedPreferences.getString(MainActivity.CHOICES, null);
+        guessRows = Integer.parseInt(choices) / 2;
+
+        //hide all guess buttons LinearLayouts
+        for (LinearLayout layout : guessLinearLayouts) {
+            layout.setVisibility(View.GONE);
+        }
+
+        //show appropriate guess buttons LinearLayouts
+        for (int row = 0; row < guessRows; row++) {
+            guessLinearLayouts[row].setVisibility(View.VISIBLE);
+        }
+    }
+
+    //update world regions for quiz based on values in SharedPreferences
+    public void updateRegions(SharedPreferences sharedPreferences) {
+        regionsSet = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
+    }
+
+    //set up and start the next quiz
+    public void resetQuiz() {
+        // use AssetManager to get image file names for enabled regions
+        AssetManager assets = getActivity().getAssets();
+        fileNameList.clear();
+
+        try {
+            //loop through each region
+            for (String region : regionsSet) {
+                String[] paths = assets.list(region);
+
+                for (String path : paths) {
+                    fileNameList.add(path.replace(".png", ""));
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error Loading image file names", e);
+        }
+
+        correctAnswers = 0;
+        totalGuesses = 0;
+        quizCountriesList.clear();
+
+        int flagCounter = 1;
+        int numberOfFlags = fileNameList.size();
+
+        // adds FLAGS_IN_QUIZ random file names to the quizCountriesList
+        while (flagCounter <= FLAGS_IN_QUIZ) {
+            int randomIndex = random.nextInt(numberOfFlags);
+            //get the random file name
+            String fileName = fileNameList.get(randomIndex);
+
+            //if the region is enabled and it hasn't already been chosen
+            if (!quizCountriesList.contains(fileName)) {
+                quizCountriesList.add(fileName);
+                ++flagCounter;
+            }
+        }
+
+        loadNextFlag();
+    }
+
+    private void loadNextFlag() {
+        //get file name of the next flag and remove from list
+        String nextImage = quizCountriesList.remove(0);
+        correctAnswer = nextImage;
+        answerTextView.setText("");
+
+        questionNumberTextView.setText(getString(R.string.question, (correctAnswers + 1), FLAGS_IN_QUIZ));
+
+        //extract tje region from  the next image's name
+        String region = nextImage.substring(0, nextImage.indexOf("-"));
+
+        AssetManager assets = getActivity().getAssets();
+
+        // get an InputStream to the asset representing the next flag
+        // and try to use the InputStream
+
+        try (InputStream stream = assets.open(region + "/" + nextImage + ".png")) {
+            Drawable flag = Drawable.createFromStream(stream, nextImage);
+            flagImageView.setImageDrawable(flag);
+            animateFlagChange(false);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading " + nextImage, e);
+        }
+
+        Collections.shuffle(fileNameList);
+
+        //put the correct answer at the end of fileNameList
+        int correct = fileNameList.indexOf(correctAnswer);
+        fileNameList.add(fileNameList.remove(correct));
+
+        // add 2, 4, 6 or 8 guess Buttons based on the value of guessRows
+        for (int row = 0; row < guessRows; row++) {
+            // place Buttons in currentTableRow
+            for (int column = 0; column < guessLinearLayouts[row].getChildCount(); column++) {
+                // get reference to Button to configure
+                Button newGuessButton = (Button) guessLinearLayouts[row].getChildAt(column);
+                newGuessButton.setEnabled(true);
+
+                // get country name and set it as newGuessButton's text
+                String filename = fileNameList.get((row * 2) + column);
+                newGuessButton.setText(getCountryName(filename));
+            }
+        }
+
+        // randomly replace one Button with the correct answer
+        int row = random.nextInt(guessRows);
+        int column = random.nextInt(2);
+        LinearLayout randomRow = guessLinearLayouts[row];
+        String countryName = getCountryName(correctAnswer);
+        ((Button) randomRow.getChildAt(column)).setText(countryName);
+    }
+
+    // parses the country flag file name and returns the country name
+    private String getCountryName(String name) {
+        return name.substring(name.indexOf('-') + 1).replace('_', ' ');
+    }
+
+    // animates the entire quizLinearLayout on or off screen
+    private void animateFlagChange(boolean animateOut) {
+        if (correctAnswers == 0) {
+            return;
+        }
+
+        // calculate center x and center y
+        int centerX = (quizLinearLayout.getLeft() + quizLinearLayout.getRight()) / 2;
+        int centerY = (quizLinearLayout.getTop() + quizLinearLayout.getBottom()) / 2;
+
+        //calculate animation radius
+        int radius = Math.max(quizLinearLayout.getHeight(), quizLinearLayout.getWidth());
+
+        Animator animator;
+        if (animateOut) {
+            //create circular reveal animation
+            animator = ViewAnimationUtils.createCircularReveal(quizLinearLayout, centerX, centerY, radius, 0);
+            animator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loadNextFlag();
+                }
+            });
+        } else {
+            animator = ViewAnimationUtils.createCircularReveal(quizLinearLayout, centerX, centerY, 0, radius);
+        }
+
+        animator.setDuration(500);
+        animator.start();
+    }
+
+    private View.OnClickListener guessButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button guessButton = (Button) view;
+            String guess = guessButton.getText().toString();
+            String answer = getCountryName(correctAnswer);
+            ++totalGuesses;
+
+            if (guess.equals(answer)) {
+                ++correctAnswers;
+                answerTextView.setText(getString(R.string.answer_text_view_correct_answer_text));
+                answerTextView.setTextColor(getResources().getColor(R.color.correct_answer,
+                        getContext().getTheme()));
+
+                disableButtons(); // disable all guess Buttons
+
+                // if the user has correctly identified all the FLAGS_IN_QUIZ flags
+                if (correctAnswers == FLAGS_IN_QUIZ) {
+
+                    AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+                    alert.setTitle("quiz results");
+                    alert.setMessage(getString(R.string.results,
+                            totalGuesses,
+                            (100 * FLAGS_IN_QUIZ) / (double) totalGuesses));
+
+                    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            resetQuiz();
+                        }
+                    });
+
+                    alert.setCancelable(false);
+
+                    alert.show();
+                } else { // answer is correct but quiz is not over
+                    // load the next flag after a 2-second delay
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            animateFlagChange(true);
+                        }
+                    }, 1000);
+                }
+            } else {
+                // answer was incorrect
+                flagImageView.startAnimation(shakeAnimation);
+
+                answerTextView.setText(R.string.incorrect_answer);
+                answerTextView.setTextColor(getResources().getColor(R.color.incorrect_answer,getContext().getTheme()));
+                guessButton.setEnabled(false);
+            }
+        }
+    };
+
+    private void disableButtons() {
+        for (int row = 0; row < guessRows; row++) {
+            LinearLayout guessRow = guessLinearLayouts[row];
+            for (int column = 0; column < guessRow.getChildCount(); column++) {
+                guessRow.getChildAt(column).setEnabled(false);
+            }
+        }
     }
 }
